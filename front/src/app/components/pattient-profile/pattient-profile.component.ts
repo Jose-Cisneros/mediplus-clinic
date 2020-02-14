3b826b3f85de42398f6c428d30f394935ec1fe3c
Santiago Rudolf <santirudolf@gmail.com>
2020-02-14 18:08:33 -0300

    Correccion

--- front/src/app/components/pattient-profile/pattient-profile.component.ts
+++ front/src/app/components/pattient-profile/pattient-profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pattient-profile',
   templateUrl: './pattient-profile.component.html',
-  styleUrls: ['./pattient-profile.component.css']
+  styleUrls: ['./pattient-profile.component.scss']
 })
 export class PattientProfileComponent implements OnInit {
 
